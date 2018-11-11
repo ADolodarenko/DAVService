5fe5d04847e9a5645e1eb9e54e044bbf512892f8
ADolodarenko <adolodar@gmail.com>
2018-11-11 21:59:38 +0300

    Added getAvailableLocales method to ResourceManager class.

--- src/org/dav/service/util/ResourceManager.java
+++ src/org/dav/service/util/ResourceManager.java
@@ -2,11 +2,13 @@ package org.dav.service.util;
 
 import javax.swing.*;
 import java.io.File;
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public interface ResourceManager
 {
+    List<Locale> getAvailableLocales();
     Locale getCurrentLocale();
     void setCurrentLocale(Locale locale);
     ResourceBundle getBundle();
